29e5495b556f3f53a3693d1c71191577d01da070
github username <github email address>
2021-07-07 21:46:50 -0400

    practice writing classes in ruby

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title = '', author = '', page = '', genre = '')
+    @title = title
+    @author = author
+    @page_count = page
+    @genre = genre
+  end
+
+  def turn_page 
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
